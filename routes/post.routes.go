3a66f7c33deb9dc4c0613d134b48572404b07610
relieyanhilman <relieyanhilman93@gmail.com>
2023-03-18 11:08:52 +0700

    add routes

--- routes/post.routes.go
+++ routes/post.routes.go
@@ -1,1 +1,27 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/relieyanhilman/basic-go-app/controllers"
+)
+
+type PostRouteController struct {
+	postController controllers.PostController
+}
+
+func NewRoutePostController(postController controllers.PostController) PostRouteController {
+	return PostRouteController{postController}
+}
+
+func (pc *PostRouteController) PostRoute(rg *gin.RouterGroup) {
+
+	router := rg.Group("posts")
+
+	//add this after configure the user authentication
+	// router.Use(middleware.DeserializeUser())
+	router.POST("/", pc.postController.CreatePost)
+	router.GET("/", pc.postController.FindPosts)
+	router.PUT("/:postId", pc.postController.UpdatePost)
+	router.GET("/:postId", pc.postController.FindPostById)
+	router.DELETE("/:postId", pc.postController.DeletePost)
+}
